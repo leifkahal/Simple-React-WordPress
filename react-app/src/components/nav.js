e982f72ef135df36110700aa79addba672e1dca0
Leif Kahal <leifkahal@thehundo.com>
2020-01-07 15:47:05 +0000

    handles top navigation

--- react-app/src/components/nav.js
+++ react-app/src/components/nav.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav'; 
 
 const Navibar = () => {
 
