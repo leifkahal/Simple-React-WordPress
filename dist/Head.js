30500bc7ea9e8deebf9348b141858e497b8c18e2
Leif Kahal <leifkahal@thehundo.com>
2020-08-12 21:14:14 -0400

    ...

--- dist/Head.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-
-const Head = props => {
-  return /*#__PURE__*/React.createElement(Helmet, null, /*#__PURE__*/React.createElement("title", null, props.title + ' | ' + global.Configs.companyTitle));
-};
-
-export default Head;
